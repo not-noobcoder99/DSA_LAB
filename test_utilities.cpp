dbbbbd7ef35d4193fb769c353551e95077be45f7
Umar Tanveer <151952538+not-noobcoder99@users.noreply.github.com>
2025-09-09 15:16:25 +0500

    Add smoke test for utilities::add

--- test_utilities.cpp
+++ test_utilities.cpp
@@ -1,3 +1,9 @@
-//
-// Created by hp on 09/09/2025.
-//
+#include <iostream>
+#include "utilities.h"
+using namespace std;
+
+
+int main() {
+    cout << "add(2,3) == 5: " << (add(2,3) == 5 ? "PASS" : "FAIL") << endl;
+    return 0;
+}
